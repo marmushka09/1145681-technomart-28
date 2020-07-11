42cda2f4ef50ae5b12acda2b9998f589531c0123
marmushka09 <64732263+marmushka09@users.noreply.github.com>
2020-07-11 20:39:01 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+
 document.querySelector('.write-us-link').onclick = function() {
 	document.getElementById('write-us').style.display = 'block';
 	return false;
@@ -24,33 +26,4 @@ let blocks = document.querySelectorAll('.services li');
 document.querySelector('.services li.' + id).classList.add('active');
 		});
 });
-	
-
-
-
-
-document.querySelectorAll('.promo-slider .promo-slider-head .promo-slogan').showSlide = function(event) {
-	event.changeSlide(event, index);
-
-let slideShow = document.querySelector('.promo-slider'); 
-let slideHead = document.querySelector('.promo-slider-head');
-let slideText = document.querySelector('.promo-slogan') {
-			isAutoplay: true,
-        	directionAutoplay: 'next',
-        	delayAutoplay: 5000,
-        	isPauseOnHover: true;
-});
-
-	config = {
-        	isAutoplay: true, // автоматическая смена слайдов
-        	directionAutoplay: 'next', // направление смены слайдов
-        	delayAutoplay: 5000, // интервал между автоматической сменой слайдов
-        	isPauseOnHover: true // устанавливать ли паузу при поднесении курсора к слайдеру
-      });
-			
-	function changeSlide(event, index) {
-    	let slideShow = document.querySelector('.promo-slider');
-		let slideHead = document.querySelector('.promo-slider-head');
-		let slideText = document.querySelector('promo-slogan') {
-        	
-    };
+});	
